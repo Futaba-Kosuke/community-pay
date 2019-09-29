b94d365d4291292f0d8dda46fb948ee357648c3e
Futaba-Kosuke <iemon.ayataka.f@gmail.com>
2019-09-29 20:32:18 +0900

    [update] TransitionButton.vueの基本機能が完成しました。 #2
    Homeから他のページに遷移する為のボタンの基本機能が完成しました。
    デザインはまだ完成していません。

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,21 +1,26 @@
 <template>
   <div class="home">
     <HeaderItem/>
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!-- <img alt="Vue logo" src="../assets/logo.png"> -->
+    <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+    <TransitionButton title="create_coin" link="/create_coin"/>
+    <TransitionButton title="use_coin" link="/use_coin"/>
+    <TransitionButton title="qr_reader" link="/qr_reader"/>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+// import HelloWorld from '@/components/HelloWorld.vue'
 import HeaderItem from '@/components/HeaderItem.vue'
+import TransitionButton from '@/components/TransitionButton.vue'
 
 export default {
   name: 'home',
   components: {
-    HelloWorld,
+    // HelloWorld,
     HeaderItem,
+    TransitionButton,
   }
 }
 </script>

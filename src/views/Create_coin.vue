a1b4ee2d00b560f1e0fd1c6b2ed27bcce82a1eab
Futaba-Kosuke <iemon.ayataka.f@gmail.com>
2019-09-29 16:24:37 +0900

    [update] ヘッダーが各頁に表示されるようにしました
    ヘッダーが各頁に表示できるように、各ページのコンポ―ネントを編集しました。

--- src/views/Create_coin.vue
+++ src/views/Create_coin.vue
@@ -1,5 +1,17 @@
 <template>
-  <div class="about">
+  <div class="create_coin">
+    <HeaderItem/>
     <h1>This is a create_coin page</h1>
   </div>
 </template>
+
+<script>
+import HeaderItem from "@/components/HeaderItem"
+
+export default {
+  name: 'about',
+  components: {
+    HeaderItem,
+  }
+}
+</script>

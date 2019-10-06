3a2c595f4932327a3ca0dbe086827c517dde311d
Futaba-Kosuke <iemon.ayataka.f@gmail.com>
2019-10-06 13:47:13 +0900

    [update] コイン使用画面にボタンを表示しました。 #3
    コイン使用画面にボタンを表示し、次の操作を実装しやすくしました。

---
+++ src/components/ButtonList.vue
@@ -0,0 +1,19 @@
+<template>
+  <v-card>
+    <Button v-for="l in list" v-bind:key="l.name" :msg="l.name"/>
+  </v-card>
+</template>
+
+<script>
+import Button from './CoinButton'
+
+export default {
+  name: 'button_list',
+  components: {
+    Button,
+  },
+  props: {
+    list: Array,
+  }
+}
+</script>
--- src/views/Use_coin.vue
+++ src/views/Use_coin.vue
@@ -1,14 +1,25 @@
 <template>
   <div class="use_coin">
     <h1>This is a use_coin page</h1>
+    <Buttons :list="list"/>
   </div>
 </template>
 
 <script>
+import Buttons from '../components/ButtonList'
 
 export default {
   name: 'use_coin',
   components: {
-  }  
+    Buttons,
+  },
+  data: () => {
+    return {
+      list: [
+        { name: 'a'},
+        { name: 'b'},
+      ]
+    }
+  }
 }
 </script>

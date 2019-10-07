43b50c7b0220da1e19e34f45b47825de774aa074
Futaba-Kosuke <iemon.ayataka.f@gmail.com>
2019-10-07 19:17:59 +0900

    [update] TransitionBar.vueが仮完成しました。
    TransitionBar.vueが仮完成しました。

---
+++ src/components/TransitionBar.vue
@@ -0,0 +1,11 @@
+<template>
+  <v-row
+    justify="center"
+    no-gutters
+  >
+    <v-btn to="/" text color="white" class="my-2" exact>HOME</v-btn>
+    <v-btn to="/use_coin" text color="white" class="my-2" exact>使用する</v-btn>
+    <v-btn to="/create_coin" text color="white" class="my-2" exact>作成する</v-btn>
+    <v-btn to="/qr-reader" text color="white" class="my-2" exact>QR</v-btn>
+  </v-row>
+</template>

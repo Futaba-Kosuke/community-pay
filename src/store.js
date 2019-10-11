f788da6cd49c648de2463a6f07d40aadcde2ab59
Futaba-Kosuke <iemon.ayataka.f@gmail.com>
2019-10-12 00:42:17 +0900

    [add] コインの情報と操作用関数を定義しました #4
    今後はVuexでコインの管理を行います。コイン使用画面への反映は後ほど行います。

--- src/store.js
+++ src/store.js
@@ -5,10 +5,18 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
-
+    active_coin: [],
+    negative_coin: [],
   },
   mutations: {
-
+    enableCoin (state, coin) {
+      state.active_coin.push(coin);
+      state.negative_coin = state.negative_coin.filter((n) => n.name !== coin.name);
+    },
+    disableCoin (state, coin) {
+      state.negative_coin.push(coin);
+      state.active_coin = state.active_coin.filter((n) => n.name !== coin.name);
+    },
   },
   actions: {
 

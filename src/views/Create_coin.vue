6d4853584a13708b31d001feb1913690e0d89057
Futaba-Kosuke <iemon.ayataka.f@gmail.com>
2019-10-12 02:13:09 +0900

    [update] コインのデータをVuexに受け渡せるようにしました。 #4
    特に無し

--- src/views/Create_coin.vue
+++ src/views/Create_coin.vue
@@ -1,37 +1,50 @@
 <template>
   <v-container class="create_coin">
       <v-text-field
-        v-model="coin_name"
+        v-model="coin.name"
         label="コイン名"
       />
     <v-row>
       <v-col>
         <v-text-field
-          :title="coin_name"
+          v-model="coin.yen"
           label="日本円"
         />
       </v-col>
       <v-col>
         <v-text-field
-          :label="coin_name"
+          v-model="coin.coin"
+          :label="coin.name"
         />
       </v-col>
     </v-row>
     <v-textarea
+      v-model="coin.explanation"
       label="説明"
     />
+    <v-btn @click="createCoin()">この設定でコインを作成する</v-btn>
   </v-container>
 </template>
 
 <script>
-
 export default {
   name: 'about',
   components: {
   },
   data: function() {
     return {
-      coin_name: ''
+      coin: {
+        name: '',
+        yen: '',
+        coin: '',
+        explanation: '',
+      }
+    }
+  },
+  methods: {
+    createCoin: function () {
+      this.$store.commit('disableCoin', this.coin);
+      console.log(this.$store.state.negative_coin);
     }
   }
 }

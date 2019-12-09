2c4fa26f93ff4c54e8520c5d097f2df89ebe1388
コン研 <conken@matsukubojunnoMacBook-Air.local>
2019-12-09 16:50:19 +0900

    [Fix] アドレスを変更しました。 #5

--- src/components/TransitionBar.vue
+++ src/components/TransitionBar.vue
@@ -6,6 +6,6 @@
     <v-btn to="/" text color="white" class="my-2" exact>HOME</v-btn>
     <v-btn to="/use_coin" text color="white" class="my-2" exact>使用する</v-btn>
     <v-btn to="/create_coin" text color="white" class="my-2" exact>作成する</v-btn>
-    <v-btn to="/qr-reader" text color="white" class="my-2" exact>QR</v-btn>
+    <v-btn to="/qr_reader" text color="white" class="my-2" exact>QR</v-btn>
   </v-row>
 </template>

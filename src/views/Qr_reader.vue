a7158a29282c3a05d7d16d3a63e1d08951c220c5
コン研 <conken@matsukubojunnoMacBook-Air.local>
2019-12-10 17:30:27 +0900

    [add] QRコードの読み込み画面の作成 #5

--- src/views/Qr_reader.vue
+++ src/views/Qr_reader.vue
@@ -1,14 +1,29 @@
-<template>
-  <div class="qr_reader">
-    <h1>This is a qr_reader page</h1>
+<template>  <!--この下にhtml-->
+  <div>
+    <div class="qr_reader">
+      <h1>This is a qr_reader page</h1>
+    </div>
+    <qrcode-stream @decode="onDecode"></qrcode-stream>
   </div>
 </template>
 
 <script>
+import { QrcodeStream } from 'vue-qrcode-reader'
 
 export default {
-  name: 'qr_reader',
+  // name: 'qr_reader',
   components: {
-  } 
+    QrcodeStream,
+  },
+  methods: {
+    // onDecode: (content) => { 
+    //   console.log("OK!")}
+    
+    onDecode (decodedString) {
+    // ...
+  }
+ }
 }
-</script>>
+</script>
+
+<style></style>

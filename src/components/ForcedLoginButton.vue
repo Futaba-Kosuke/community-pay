fec2b1bd4051c2d708e6c7c77c22c8f5a8cb2218
Futaba-Kosuke <iemon.ayataka.f@gmail.com>
2019-12-16 22:56:40 +0900

    [add] 強制ログインボタンを作成しました #29

---
+++ src/components/ForcedLoginButton.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="my-2">
+    <v-btn to="/login_form" x-large class="start_button" color="rgb(71, 140, 234)">ログインしてください</v-btn>
+  </div>
+</template>
+
+<style scoped>
+.start_button {
+  position: absolute;
+  top: 40%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+
+  padding: 15px 30px;
+  text-align: center;
+  width: 300px;
+  font-size: 20px;
+
+  color: white;
+}
+</style>

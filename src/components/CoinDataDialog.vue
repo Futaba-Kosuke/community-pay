f14be088c49e6cd4de191ab080b0bf2698df9447
Futaba-Kosuke <iemon.ayataka.f@gmail.com>
2019-12-20 00:58:09 +0900

    [add] ランダムなkeyによるQR生成、データベースへの登録を実装しました #35
    データベースにkeyを登録して、そのkeyでQRコードを生成します。
    QRに埋め込まれたkeyとデータベースに上がっているkeyを比較して、一致したら決済の許可を出します。

--- src/components/CoinDataDialog.vue
+++ src/components/CoinDataDialog.vue
@@ -12,6 +12,11 @@
         </v-btn>
         <v-toolbar-title>{{ coin_data.name }}</v-toolbar-title>
       </v-toolbar>
+      <v-card-actions>
+        <v-col><v-btn @click="generateQR(true)" style="height: 100px;" block color="primary">ペイ</v-btn></v-col>
+        <v-col><v-btn @click="generateQR(false)" style="height: 100px;" block color="primary">チャージ</v-btn></v-col>
+      </v-card-actions>
+      <vue-qrcode v-if="qr_str" :value="qr_str" :options="option" tag="img"></vue-qrcode>
       <v-list three-line subheader>
         <v-subheader>これはコインの使用・詳細確認画面です</v-subheader>
         <v-list-item>
@@ -44,21 +49,80 @@
 </template>
 
 <script>
+import firebase from 'firebase'
+import VueQrcode from '@chenfengyuan/vue-qrcode'
+
 export default {
+  components: {
+    VueQrcode,
+  },
   props: {
     coin_data: Object,
     dialog_flag: Boolean,
   },
+  data () {
+    return {
+      qr_str: '',
+      option: {
+        errorCorrectionLevel: "M",
+        maskPattern: 0,
+        margin: 10,
+        width: 400,
+        color: {
+          dark: "#000000FF",
+          light: "#FFFFFFFF"
+        },
+      },
+
+    }
+  },
   methods: {
     closeThis() {
       this.$emit('closeThis')
+    },
+    generateQR: function (mode) {
+      // ランダム文字列の生成。DBのランダム文字列と、QRのランダム文字列が同値の時に決済の許可を出す。
+      const LENGTH = 20
+      const WORDS = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+      const generateRandomStr = () => {
+        let r = "";
+        for(let i = 0; i < LENGTH; i++){
+          r += WORDS[Math.floor(Math.random() * WORDS.length)];
+        }
+        return r;
+      }
+
+      const key = generateRandomStr()
+
+      // カレントユーザーのID
+      const uid = firebase.auth().currentUser.uid
+
+      // DBに上げるデータの生成 ~ データベースへの登録
+      const key_data = {
+        key: key,
+        generation_time: new Date(),
+      }
+      const db = firebase.firestore()
+      db.collection('user').doc(uid).update({
+        key: key_data,
+      })
+
+      // QRデータの生成 ~ QRコードの生成
+      const qr_data = JSON.stringify({
+        mode: mode,
+        uid: uid,
+        key: key,
+      })
+
+      this.qr_str = qr_data
+
     }
   },
   computed: {
     deadline: function() {
       if (this.coin_data.deadline === '無制限') {
         return this.coin_data.deadline
-      } 
+      }
       return '最後の使用から ' + this.coin_data.deadline
     }
   }
@@ -71,5 +135,9 @@ export default {
 }
 .item-title {
   font-size: 25px;
+}
+.QR {
+  text-align: center;
+  size: 300px;
 }
 </style>

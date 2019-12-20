6f1c747369e279be67e777d64a22495684872731
Futaba-Kosuke <iemon.ayataka.f@gmail.com>
2019-12-20 23:45:03 +0900

    [add] HOME.vueを無効化しました
    完成してたら有効化するつもりでした。悲しみ

--- src/components/TransitionBar.vue
+++ src/components/TransitionBar.vue
@@ -3,9 +3,8 @@
     justify="center"
     no-gutters
   >
-    <v-btn to="/" text color="white" class="my-2" exact>HOME</v-btn>
-    <v-btn to="/use_coin" text color="white" class="my-2" exact>使用する</v-btn>
+    <v-btn to="/" text color="white" class="my-2" exact>使用する</v-btn>
     <v-btn to="/create_coin" text color="white" class="my-2" exact>作成する</v-btn>
-    <v-btn to="/qr_reader" text color="white" class="my-2" exact>QR</v-btn>
+    <v-btn to="/qr_reader" text color="white" class="my-2" exact>運営する</v-btn>
   </v-row>
 </template>
